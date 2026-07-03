220f79c345e3e8de88eeae1fce1b2aae1ff3be86
Asker <cxzcnmt99@gmail.com>
2026-07-03 17:25:39 +0300

    health_handler

--- cmd/forum/main.go
+++ cmd/forum/main.go
@@ -1,9 +1,13 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"stepik.leoscode.http/internal/handlers"
+)
 
 func main() {
 	r := gin.Default()
 
+	r.GET("/internal/v1/health", handlers.Health)
 	r.Run(":8080")
 }
---
+++ internal/handlers/health.go
@@ -0,0 +1,16 @@
+package handlers
+
+import (
+	"net/http"
+	"github.com/gin-gonic/gin"
+)
+
+func Health(c *gin.Context) {
+	reqID := c.GetHeader("X-Request-Id")
+	if reqID != "" {
+		c.Header("X-Request-Id", reqID)
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"status": "ok",
+	})
+}
