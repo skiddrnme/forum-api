dca06e731d55d81f036360c985025ba80ad87d2e
Asker <cxzcnmt99@gmail.com>
2026-08-03 17:02:28 +0300

    at_serv

--- internal/service/attachment.go
+++ internal/service/attachment.go
@@ -3,7 +3,6 @@ package service
 import (
 	"errors"
 	"fmt"
-	"path/filepath"
 	"strconv"
 	"sync"
 	"time"
@@ -12,49 +11,49 @@ import (
 	forum "stepik.leoscode.http/internal/gen/api"
 )
 
-
-type AttachmentService struct{
-	attachments map[int64]forum.Attachment
+type AttachmentService struct {
+	attachments   map[openapi_types.UUID]forum.Attachment
 	threadService *ThreadService
-	nextID int
-	mu sync.Mutex
+	mu            sync.Mutex
 }
 
-func NewAttachmentService() *AttachmentService{
+func NewAttachmentService(threadService *ThreadService) *AttachmentService {
 	return &AttachmentService{
-		attachments: make(map[int64]forum.Attachment),
-		nextID: 1,
+		attachments:   make(map[openapi_types.UUID]forum.Attachment),
+		threadService: threadService,
 	}
 }
 
-
-func (a *AttachmentService) UploadAttachment(threadID string, userID openapi_types.UUID, file string) (forum.Attachment, error){
+func (a *AttachmentService) UploadAttachment(threadID string, userID openapi_types.UUID, file string, size int64, mimeType forum.AttachmentMimeType) (forum.Attachment, error) {
 	a.mu.Lock()
 	defer a.mu.Unlock()
 
 	threadIDInt, err := strconv.ParseInt(threadID, 10, 64)
-	if err != nil{
+	if err != nil {
 		return forum.Attachment{}, fmt.Errorf("invalid thread_id: %w", err)
 	}
 	entry, exists := a.threadService.threads[threadIDInt]
-	if !exists{
+	if !exists {
 		return forum.Attachment{}, errors.New("thread not found")
 	}
-	if entry.AuthorId != userID{
+	if entry.AuthorId != userID {
 		return forum.Attachment{}, errors.New("user mismatch: cannot modify another user's thread")
 	}
 
+	attachment := a.createNewAttachment(file, threadIDInt, size, mimeType)
+	a.attachments[attachment.Id] = attachment
 
-
-
-	return forum.Attachment{}, nil
+	return attachment, nil
 }
 
-
-func (a *AttachmentService) createNewAttachment(filename string, threadID int64) forum.Attachment {
+func (a *AttachmentService) createNewAttachment(filename string, threadID int64, size int64, mimeType forum.AttachmentMimeType) forum.Attachment {
 	attachment := forum.Attachment{
 		CreatedAt: time.Now(),
-		Filename: filename,
-		
+		Filename:  filename,
+		MimeType:  mimeType,
+		Size:      size,
+		ThreadId:  threadID,
+		Url:       nil,
 	}
+	return attachment
 }
